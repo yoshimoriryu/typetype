6a5558f267d58eadfac4249d2a1b584d4cc15b24
Hamzah Chairil Salim <13517051@std.stei.itb.ac.id>
2024-01-23 14:10:01 +0700

    feat: add react typing test copy from g4g:https://www.geeksforgeeks.org/typing-speed-tester-using-react/

---
+++ react-fe/src/App.js
@@ -0,0 +1,15 @@
+
+// App.js 
+  
+import './App.css'; 
+import SpeedTypingGame from './components/SpeedTypingGame'; 
+  
+function App() { 
+  return ( 
+    <div className="App"> 
+      <SpeedTypingGame /> 
+    </div> 
+  ); 
+} 
+  
+export default App; 
